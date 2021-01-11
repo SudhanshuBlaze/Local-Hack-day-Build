600d4047bcf2145e41682961c040c3e8f940fb8f
Sudhanshu Kumar <63813872+SudhanshuBlaze@users.noreply.github.com>
2021-01-12 01:26:06 +0530

    added button

--- Day-2/Random Number Generator/script.js
+++ Day-2/Random Number Generator/script.js
@@ -1,7 +1,10 @@
 
 var n=prompt("Enter the upper bound");
+function gen(n){
+    let randomNum=Math.round(Math.random()*n);
+    console.log(randomNum);
+    document.querySelector("h2").innerText="Refresh to Generate Random Number Between 0 to "+n;
+    document.querySelector("p").innerText=randomNum;
+}
 
-let randomNum=Math.round(Math.random()*n);
-console.log(randomNum);
-document.querySelector("h2").append(" "+n);
-document.querySelector("p").innerText=randomNum;
+gen(n);
