322ebac116a9030517fe0fc50d5bfb74f9f740f0
Jagat Jyoti Mishra <59306207+jagat546@users.noreply.github.com>
2021-01-13 12:55:40 +0530

    Add files via upload

---
+++ day 3/SnakeGame/src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+/*This is Beginner Snake Game made by Jagat Jyoti Mishra */
+public class Main {
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
